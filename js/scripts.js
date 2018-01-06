305e045bf591aa95f5c59a765a1ff70e36ef0880
Agnieszka Nowacka <aguo84@gmail.com>
2018-01-06 21:30:04 +0100

    Add index.html and scripts.js with the code

---
+++ js/scripts.js
@@ -0,0 +1,7 @@
+alert("Hey, let's calculate the area of triangle :)");
+var a = prompt('Enter value of base a'),
+	h = prompt('Enter value of height h'),
+	triangleArea = (a * h) / 2;
+alert("Triangle field with base a: " + a + " and height h: " + h + " is equal to: " + triangleArea);
+console.log("Triangle field with base a: " + a + " and height h: " + h + " is equal to: " + triangleArea);
+
